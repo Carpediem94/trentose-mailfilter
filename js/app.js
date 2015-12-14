ff3cf9a614572b2e8236402e4eed95602145e334
Carpediem94 <mat.carpella@gmail.com>
2015-12-14 15:41:48 +0100

    second sprint

--- js/app.js
+++ js/app.js
@@ -16,24 +16,74 @@ var MailModel = {
     * Filters out messages in the "database" that match the spam rules.
     * @return an array of messages, excluding those that match the filter rules.
     */
-    filter : function(){
-      return [];
-    }
-
-  
+    filter : function() {
+      var filter = [];
+      var control = true;
+      for(var i=0; i<this.messages.length; i++) {
+        for(var j=0; j<this.rules.length; j++) {
+          if(this.messages[i].from.indexOf(this.rules[j].from) !== -1) {
+            control = false;
+          } else if (this.messages[i].subject.indexOf(this.rules[j].subject) !== -1) {
+            control = false;
+          } 
+        }
+        
+        if(control) {
+          filter.push(this.messages[i]);
+        }
+        control = true;
+      }
+      
+      return filter;
+    } 
 };
 
 // Example of usage:
 // MailModel.init()
 // MailModel.filter() 
 //  -> ["carlo@gmail.com", "trentose2@googlegroups.com"]
-
+var octopus = {
+  init: function() {
+    MailModel.init();
+    viewMail.init();
+  },
+  
+  getMsgs: function() {
+    return MailModel.messages;
+  },
+  
+  filterMsgs: function() {
+    var msgs = MailModel.filter();
+    MailModel.messages = msgs;
+  }
+};
 
 // We suggest to use js patters. 
 // you can add here your views and controllers if you decide to do so.
-
+var viewMail = {
+  init: function() {
+    this.mailList = $('.result');
+    this.render();
+    this.runFilter();
+  },
+  
+  render: function() {
+    this.mailList.empty();
+    var msgs = octopus.getMsgs();
+    for(var i=0; i<msgs.length; i++) {
+      this.mailList.append('<li>' + msgs[i].from + '</li>');
+    }
+  },
+  
+  runFilter: function() {
+    $('.btn-filter').click(function() {
+      octopus.filterMsgs();
+      viewMail.render();
+    });
+  }
+}
 
 
 $(document).ready(function(){
-
+  octopus.init();
 });
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,5 +1,33 @@
 describe("MailModel", function() {
-  it("should..", function(){
+  it("should properly init", function() {
+    MailModel.init();
+    
+    //verifico che il valore sia definito
+    expect(rules).toBeDefined();
+    //verifico che i valori siano uguali
+    expect(MailModel.rules).toEqual(rules);
+    expect(MailModel.messages).toEqual(msgs);
+    //verifico che l'elemento sia contenuto nell'array
+    expect(rules).toContain("spam.com");
+    //verifico che la sottostringa sia contenuta
+    expect(msgs).toMatch("spam.com");
   });
- 
+  
+  it("should properly filter", function() {
+    var filter = MailModel.filter();
+    
+    //expect(filter).toEqual(['carlo@gmail.com', 'trentose2@googlegroups.com']);
+    expect(filter).not.toMatch(rules);
+  });
+  
+  it("experiment", function() {
+    //minore di
+    expect(3).toBeLessThan(5);  
+    //maggiore di
+    expect(3).toBeGreaterThan(1); 
+    expect(undefined).toBeUndefined();
+    expect(null).toBeNull();
+  });
 });
+
+//
